23278050d8282f1475463669a1ca2273472ae7c1
Zenithican <41877233+Milan1965@users.noreply.github.com>
2020-04-22 21:53:34 +0200

    DoS Protection

--- Locale/NL.php
+++ Locale/NL.php
@@ -7,6 +7,6 @@ $NL = array("Mobile" => "Mobile Scaled",
   			"soon" => "Soon",
   			"soon" => "Soon",
   			"soon" => "Soon",
-  			"soon" => "Soon",
+  			"explain" => "Je moet zoveel mogelijk x gaan refreshen, elke refresh kun je hoger worden.",
   			"Title" => "Chewie",
   			"Footer" => "Dit is gemaakt door Milan");
